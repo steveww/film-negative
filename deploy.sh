f6a1cc618cb78cd01e4f4f6702b404effc175277
SteveWW <s@steveww.org>
2025-08-13 17:02:39 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case $(uname) in
+    Darwin)
+        DESTINATION="$HOME/Library/Application Support/GIMP/2.10/scripts"
+        ;;
+    Linux)
+        DESTINATION="$HOME/.config/GIMP/2.10/scripts"
+        ;;
+    *)
+        echo "Unknown system"
+        echo "Bye"
+        exit 1
+        ;;
+esac
+
+echo "Copying script to $DESTINATION"
+copy film-negative.scm "$DESTINATION"
